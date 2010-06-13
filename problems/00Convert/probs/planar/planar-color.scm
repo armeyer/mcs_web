08a4b3139f140c5766fa014a62667b26ad780c8b
dmjones <dmj@ams.org>
2010-06-13 01:38:49 +0000

    Imported planar tutor problems

---
+++ problems/00Convert/probs/planar/planar-color.scm
@@ -0,0 +1,17 @@
+;; planar-color
+
+(define-problem
+  '(
+    (type short-answer)
+    (title "Faces of a Planar Embedding")
+    (box-width 20)
+    (questions
+     (! "<strong>How many faces are there in the following graph?</strong>"
+ (answer (3) (compare test-set)
+          (rationale 
+"
+
+"))  ) ;!
+     ) ;questions
+    ) ;'
+  );define-problem
