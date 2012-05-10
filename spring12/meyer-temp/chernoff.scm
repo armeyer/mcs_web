1ac2482f057f6408b934049b87cfbca6d6de41b3
meyer <meyer.csail@gmail.com>
2012-05-10 23:12:28 +0000

    chernoff.scm

--- spring12/meyer-temp/chernoff.scm
+++ spring12/meyer-temp/chernoff.scm
@@ -6,5 +6,8 @@
 (define (ebeta c)
   (exp (- (beta c))))
 
-;(expt (ebeta 3/2) 111/4)
-;Value: .04966349489083584
+;(pp (map (lambda (x) (list (cons 'beta x) (beta x)))
+;	 '(1 1.01 1.02 1.05 1.1 1.2 1.3 1.5 1.6 1.9 2 2.2 2.7 3 4 8 20 50 100)))
+
+;(pp (map (lambda (x) (list (cons 'ebeta x) (ebeta x)))
+;	 '(1 1.01 1.02 1.05 1.1 1.2 1.3 1.5 1.6 1.9 2 2.2 2.7 3 4 8 20 50 100)))
