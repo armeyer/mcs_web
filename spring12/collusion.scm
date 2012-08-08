19d6218e4f1ec51a19d44ea08054bc5256e388cc
meyer <meyer.csail@gmail.com>
2012-08-08 20:50:47 +0000

    withdraw PS_dependent_pairs

--- spring12/collusion.scm
+++ spring12/collusion.scm
@@ -1,14 +1,1 @@
-(+
-
-(* 1/6 5/6 5/6)     ;sole winner       payoff: 4
-
-(* 1/6 2 1/6 5/6)   ;1 of 2 winners3   payoff: 1
-
-(* 1/6 1/6 1/6)     ;3 winners         payoff: 0
-
-(* 5/6 5/6 5/6)     ;0 winners         payoff: 0
-
-(* 5/6 (+ (* 1/6 5/6 2) (* 1/6 1/6)))  payoff: -2
-                    ;at least 1 other winner
-
-)
+shit
